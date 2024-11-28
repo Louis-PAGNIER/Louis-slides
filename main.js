dd5b96e8e38187b4ea352bbba6217a04c1ea18c5
Louis <louis.pagnier@epita.fr>
2024-11-28 22:26:10 +0100

    Start Presentation

---
+++ main.js
@@ -0,0 +1,7 @@
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/black.css'
+
+import Reveal from 'reveal.js'
+
+const deck = new Reveal()
+deck.initialize({ hash: true, slideNumber: true })
